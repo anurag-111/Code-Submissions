a29ae1c40eef9c2cf2d1f18eca0df753c74abbd0
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-07-28 19:48:55 +0530

    Time: 36 ms (96.31%), Space: 31.8 MB (60.90%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,30 +1,37 @@
-// Brute force approach :
+// Optimized approach for finding 'k' closest elements to 'x' in a sorted array 'arr'.
 
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<pair<int, int>> differences;
+    vector<int> findClosestElements(std::vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size() - k;
+        
+        // Perform binary search to find the starting index of the 'k' closest elements.
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // If the difference between x and the mid element is greater than the difference
+            // between the (mid + k)th element and x, move the left pointer to mid + 1.
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                // Otherwise, move the right pointer to mid.
+                right = mid;
+            }
+        }
+        
+        // Create the result vector containing the 'k' closest elements.
         vector<int> result;
-        int size = arr.size();
-        
-        
-        for(int i = 0; i < size; i++) {
-            int difference = abs(arr[i] - x);
-            differences.push_back(make_pair(difference, arr[i]));
+        for (int i = 0; i < k; i++) {
+            result.push_back(arr[left + i]);
         }
-                                  
-        sort(differences.begin(), differences.end());                         
-        
-        // for(auto pair : differences) {
-        //     cout << pair.first << " "<< pair.second << endl;
-        // }
-        
-        for(int i = 0; i < k; i++) {
-            result.push_back(differences[i].second);
-        }
-        
-        sort(result.begin(), result.end());
         
         return result;
     }
 };
+
+// Time complexity: O(log(N - k)) - Due to the binary search where 'N' is the size of the 
+// input array 'arr' and 'k' is the number of closest elements to find.
+
+// Space complexity: O(k) - Due to the result vector that contains the 'k' 
+// closest elements. The space used by other variables is constant (O(1)).
