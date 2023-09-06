fc34569eed1ce3268c3601dc6ffa31ad0d75eabf
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-06 18:39:25 +0530

    Time: 8 ms (94.37%) | Memory: 24.1 MB (47.79%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,30 +1,25 @@
 class Solution {
 private:
-    void findInorder(TreeNode *root, vector<int> &inorderTraversal) {
+    void findNode(TreeNode *root, int k, int &count, TreeNode *&kthSmallest) {
         if(root == NULL) {
             return;
         }
 
-        findInorder(root -> left, inorderTraversal);
-        inorderTraversal.push_back(root -> val);
-        findInorder(root -> right, inorderTraversal);
+        findNode(root -> left, k, count, kthSmallest);
+        
+        count++;
+        if(count == k) {
+            kthSmallest = root;
+            return;
+        }
+
+        findNode(root -> right, k, count, kthSmallest);
     }
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> inorderTraversal;
-        findInorder(root, inorderTraversal);
-
-        // For printing the inorder traversal
-        // for(int num : inorderTraversal) {
-        //     cout << num << " ";
-        // }
-
-        return inorderTraversal[k - 1];        
+    int kthSmallest(TreeNode *root, int k) {
+        int count = 0;
+        TreeNode *kthSmallest = NULL;
+        findNode(root, k, count, kthSmallest);
+        return kthSmallest -> val;
     }
 };
-
-// In summary:
-
-// Time Complexity: O(N)
-// Space Complexity: O(N)
-// The code performs an in-order traversal of the binary tree to find the k-th smallest element, and both the time and space complexity are linearly dependent on the number of nodes in the tree.
