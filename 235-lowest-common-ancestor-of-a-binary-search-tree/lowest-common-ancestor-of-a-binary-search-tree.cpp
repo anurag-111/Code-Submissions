7d059934d2465a0849e858b3d018ac9f2e3300be
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-07 18:33:49 +0530

    Time: 23 ms (76.95%) | Memory: 23.2 MB (96.59%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,26 @@
+// Recursive Approach 
+class Solution {
+private:
+    TreeNode* calculateLCA(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if(root == NULL) {
+            return NULL;
+        }
+
+        if(root -> val > p -> val && root -> val > q -> val) {
+            return calculateLCA(root -> left, p, q);
+        } 
+        
+        if(root -> val < p -> val && root -> val < q -> val) {
+            return calculateLCA(root -> right, p, q);
+        }
+
+        return root;
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return calculateLCA(root, p, q);
+    }
+};
+
+// TC : O(N)
+// SC : O(H)
