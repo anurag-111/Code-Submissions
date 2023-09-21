4dd4998c0a63ef29d0387ee773cfed52130697ef
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-21 14:29:17 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (7.20%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-    // Base case: if b is 0, return a (no more additions to perform)
-    if (b == 0) {
-        return a;
+        if(b == 0) {
+            return a;
+        }
+        
+        int sumWithoutCarry = a ^ b;
+        int carry = (a & b) << 1;
+        
+        return getSum(sumWithoutCarry, carry);
     }
-    
-    // Add the digits without carrying, and store the carry in b
-    int sumWithoutCarry = a ^ b;
-    
-    // Calculate the carry and prepare for the next addition
-    int carry = (a & b) << 1;
-    
-    // Recursively call getSum with the updated values
-    return getSum(sumWithoutCarry, carry);
-}
-
 };
