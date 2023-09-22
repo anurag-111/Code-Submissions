b17d56742cfb2cd2edca4ef7de9f9eba3fbc2a01
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-22 15:40:35 +0530

    Time: 3 ms (40.09%), Space: 6.5 MB (10.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     double myPow(double base, int exponent) {
+        // Take the absolute value of 'exponent' to work with a positive exponent
+        int positiveExponent = abs(exponent);
+        
+        // Initialize the result to 1.0
         double result = 1.0;
-        int expo = abs(exponent);
-
-        while(expo > 0) {
-            if(expo & 1) {
+        
+        // Loop until 'positiveExponent' becomes 0
+        while (positiveExponent > 0) {
+            // If the current bit of 'positiveExponent' is 1 (i.e., it's odd), multiply 'result' by 'base'
+            if (positiveExponent & 1) {
                 result *= base;
             }
+
+            // Halve 'positiveExponent' (equivalent to right-shifting it by 1 bit) and square 'base'
+            positiveExponent >>= 1;
             base *= base;
-            expo = expo >> 1;
         }
-        return exponent > 0 ? result : 1 / result;
+        
+        // If the original exponent 'exponent' was negative, take the reciprocal of the result
+        return exponent < 0 ? 1 / result : result;
     }
 };
