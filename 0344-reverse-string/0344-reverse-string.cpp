6240ce186b61e4e182923e9f30834b61640c115c
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-23 16:34:22 +0530

    Time: 24 ms (15.67%), Space: 23.6 MB (10.47%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    // Function to reverse a vector of characters in place.
-    void reverseString(vector<char>& charVector) {
-        int leftIndex = 0;
-        int rightIndex = charVector.size() - 1;
-
-        // Swap characters from the left and right ends of the vector until they meet in the middle.
-        while (leftIndex < rightIndex) {
-            char tempChar = charVector[leftIndex];
-            charVector[leftIndex] = charVector[rightIndex];
-            charVector[rightIndex] = tempChar;
-
-            // Move to the next characters from both ends.
-            leftIndex++;
-            rightIndex--;
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
     }
 };
