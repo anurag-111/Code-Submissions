8aeb1043b252ca80ca0681c568514817cba07948
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-23 17:16:52 +0530

    Time: 19 ms (5.17%), Space: 7.9 MB (7.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,10 +1,22 @@
 class Solution {
+private:
+    bool is_alnum(char ch) {
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+    }
+    char to_lower(char ch) {
+        if((ch >= 'A' && ch <= 'Z')) {
+            return ch + 32;
+        } else {
+            return ch;
+        }
+    }
+
 public:
     bool isPalindrome(string s) {
         string newString;
         for(int i = 0; i < s.size(); i++) {
-            if(isalnum(s[i])) {
-                newString += tolower(s[i]);
+            if(is_alnum(s[i])) {
+                newString += to_lower(s[i]);
             }
         }
         for(char ch : newString) {
