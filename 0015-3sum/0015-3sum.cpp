e6df12f96529c50b1be4d4f6d492668a959a885f
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-25 02:06:05 +0530

    Time: 82 ms (95.74%), Space: 24.3 MB (66.84%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,40 +5,41 @@ public:
         sort(nums.begin(), nums.end());
 
         for(int i = 0; i < nums.size() - 2; i++) {
-            // Ignoring the duplicative 1st number
+            // Duplicate 1st
             if(i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int target = -(nums[i]);
             int left = i + 1;
             int right = nums.size() - 1;
-            
+            int target = - (nums[i]);
+
             while(left < right) {
                 int sum = nums[left] + nums[right];
                 if(sum < target) {
                     left++;
                 } else if(sum > target) {
                     right--;
-                } else {
+                } else{
                     // Found
                     triplets.push_back({nums[i], nums[left], nums[right]});
 
-                    // Ignoring the duplicative 2nd number
+                    // Duplicate 2nd
                     while(left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
 
-                    // Ignoring the duplicative 3rd number
+                    // Duplicate 3rd
                     while(left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
 
+                    // Move pointers
                     left++;
                     right--;
                 }
             }
         }
         return triplets;
-    } 
+    }
 };
