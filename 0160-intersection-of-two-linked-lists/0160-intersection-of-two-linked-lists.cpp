989468cd7cb4663399850617d8e6ca40b1ee2bdc
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-24 16:30:45 +0530

    Time: 30 ms (93.3%), Space: 14.9 MB (23.07%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,20 +1,64 @@
-// Approach 1 : 
-// TC : O (m * n)
+// Approach 1 
+// TC : O(m * n)
 // SC : O(1)
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode *compareNode;
+//         while(headA != nullptr) {
+//             compareNode = headB;
+//             while(compareNode != nullptr) {
+//                 if(headA == compareNode) {
+//                     return headA;
+//                 }
+//                 compareNode = compareNode -> next;
+//             }
+//             headA = headA -> next;
+//         }
+//         return NULL;
+//     }
+// };
 
+// Approach 2
+// TC : O(m + n)
+// SC : O(1)
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        while(headA != NULL) {
-            ListNode *compareNode = headB;
-            while(compareNode != NULL) {
-                if(headA == compareNode) {
-                    return headA;
-                }
-                compareNode = compareNode -> next;
+        ListNode *currentA = headA;
+        ListNode *currentB = headB;
+
+        while(currentA != currentB) {
+            if(currentA == NULL) {
+                // List A ends, move to List B
+                currentA = headB;
+            } else {
+                // List A continues
+                currentA = currentA -> next;
             }
-            headA = headA -> next;
+
+            if(currentB == NULL) {
+                // List B ends, move to List A
+                currentB = headA;
+            } else {
+                // List B continues
+                currentB = currentB -> next;
+            }
         }
-        return NULL;
+
+        return currentA;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
