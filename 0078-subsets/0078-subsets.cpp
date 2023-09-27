ca879e46dbd9ae3216703e2aaab7b0c3feb087da
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-09-28 00:03:47 +0530

    Time: 0 ms (100%), Space: 7.6 MB (51.44%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        int N = nums.size();
+        int subsetsCount = (1 << N);
         vector<vector<int>> subsets;
-        int mask = 0;
-        int n = nums.size();
-        int subsetsCount = (1 << n);
+        
 
         for(int mask = 0; mask < subsetsCount; mask++) {
             vector<int> subset;
-            for(int i = 0; i < n; i++) {
+            for(int i = 0; i < N; i++) {
                 if((mask & (1 << i)) != 0) {
                     subset.push_back(nums[i]);
                 }
