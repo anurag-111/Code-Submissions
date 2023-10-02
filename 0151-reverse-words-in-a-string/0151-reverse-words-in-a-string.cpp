56d90514976cb9a78fba3d06b2a6ea441a18f0ae
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-02 19:05:30 +0530

    Time: 11 ms (18.12%), Space: 47.3 MB (6.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
-        string temp = "";
-        string ans = "";
+        stack<string> wordStack; // Stack to store words
+        string reversedString = ""; // The final reversed string
+        string currentWord = ""; // Temporary variable to store a word
 
-        for (char c : s) {
-            if (c == ' ') {
-                if (!temp.empty())
-                    st.push(temp);
-                temp = "";
+        for (char ch : s) {
+            if (ch == ' ') { // Check for space to separate words
+                if (!currentWord.empty()) {
+                    wordStack.push(currentWord); // Push the word onto the stack
+                }
+                currentWord = ""; // Reset the temporary word
             } else {
-                temp += c;
+                currentWord += ch; // Build the current word character by character
             }
         }
 
-        ans += temp;
+        // Add the last word (if any) to the reversed string
+        reversedString += currentWord;
 
-        while (!st.empty()) {
-            ans += " " + st.top();
-            st.pop();
+        // Reconstruct the reversed string by popping words from the stack
+        while (!wordStack.empty()) {
+            reversedString = reversedString + ' ' + wordStack.top(); // Append a space and the top word from the stack
+            wordStack.pop(); // Pop the word from the stack
         }
 
-        if (!ans.empty() && ans[0] == ' ')
-            ans = ans.substr(1);
+        // Remove leading space (if any) from the reversed string
+        if (!reversedString.empty() && reversedString[0] == ' ') {
+            reversedString = reversedString.substr(1);
+        }
 
-        return ans;
+        return reversedString; // Return the reversed string
     }
 };
 
+// TC : O(N)
+// SC : O(N)
