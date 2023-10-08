12a92c28d46c50be13feb7244693dfe838e5c33e
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-09 01:09:54 +0530

    Time: 37 ms (6.26%), Space: 8.7 MB (20.83%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m;
-        
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
-        
-        int ans = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(m[s[i]] < m[s[i+1]]){
-                ans -= m[s[i]];
-            }
-            else{
-                ans += m[s[i]];
+        // Create a mapping of Roman symbols to their integer values
+        unordered_map<char, int> symbolToValue;
+        symbolToValue['I'] = 1;
+        symbolToValue['V'] = 5;
+        symbolToValue['X'] = 10;
+        symbolToValue['L'] = 50;
+        symbolToValue['C'] = 100;
+        symbolToValue['D'] = 500;
+        symbolToValue['M'] = 1000;
+
+        int result = 0;
+
+        // Traverse the Roman numeral string
+        for (int i = 0; i < s.size(); i++) {
+            char currentSymbol = s[i];
+            char nextSymbol = (i + 1 < s.size()) ? s[i + 1] : ' '; // Handle the last character
+            cout << "next symbol : " << nextSymbol << " ";
+            // If the value of the current symbol is less than the value of the next symbol,
+            // subtract the current symbol's value from the result, as it represents a subtraction case.
+            if (symbolToValue[currentSymbol] < symbolToValue[nextSymbol]) {
+                result -= symbolToValue[currentSymbol];
+            } else {
+                // Otherwise, add the current symbol's value to the result.
+                result += symbolToValue[currentSymbol];
             }
         }
-        return ans;
+
+        return result;
     }
 };
+
+
+// TC : O(N)
+// SC : O(1)
