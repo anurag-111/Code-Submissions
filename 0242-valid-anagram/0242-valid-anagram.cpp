214b0ec86bc25960ea04fd704a81945c7b16c06f
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-09 01:49:12 +0530

    Time: 5 ms (82.75%), Space: 7.6 MB (30.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) {
+        // Check if the lengths of both strings are not equal,
+        // which means they cannot be anagrams.
+        if (s.size() != t.size()) {
             return false;
         }
         
-        vector<int> freq(26, 0);
-        for(int i = 0; i < s.size(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
+        // Create an array to store the frequency of each lowercase letter.
+        vector<int> letterFrequency(26, 0);
+
+        // Iterate through both strings simultaneously.
+        for (int i = 0; i < s.size(); i++) {
+            // Increment the frequency count for the current letter in string s.
+            letterFrequency[s[i] - 'a']++;
+
+            // Decrement the frequency count for the current letter in string t.
+            letterFrequency[t[i] - 'a']--;
         }
         
-        for(int num : freq) {
-            if(num != 0) {
-                return false;
+        // Check if all frequencies in the array are zero.
+        for (int freq : letterFrequency) {
+            if (freq != 0) {
+                return false; // If not, they are not anagrams.
             }
         }
         
-        return true;
-     }
+        return true; // If all frequencies are zero, they are anagrams.
+    }
 };
+
+
+// TC : O(N)
+// SC : O(26) -> O(1)
