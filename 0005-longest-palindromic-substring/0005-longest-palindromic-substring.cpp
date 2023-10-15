d64cfcdcd11c45b4f4a5fd0264a2aead5d23c95f
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-15 12:11:43 +0530

    Time: 633 ms (21.45%), Space: 23.2 MB (34.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,39 +1,33 @@
 class Solution {
-private: 
-    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-        if(i == j){
-            return dp[i][j] = true;
-        }
-        if(j-i == 1){
-            if(s[i] == s[j]){
-                return dp[i][j] = true;
-            }
-            else{
-                return dp[i][j] = false;
-            }
-        }
-        if(s[i] == s[j] && dp[i+1][j-1] == true){
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
-        }
-    }
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int startIndex = 0; int maxlen = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int g=0; g<n; g++){
-            for(int i=0, j=g; j<n; i++, j++){
-                solve(dp, i, j, s);
-                if(dp[i][j] == true){
-                    if(j-i+1 > maxlen){
-                        startIndex = i;
-                        maxlen = j-i+1;
+    string longestPalindrome(string str) {
+        vector<vector<bool>> dp(str.size(), vector<bool>(str.size(), false));
+        int length = 0;
+        int start = 0;
+        for(int gap = 0; gap < str.size(); gap++) {
+            for(int i = 0, j = gap; j < str.size(); i++, j++) {
+                if(gap == 0) {
+                    dp[i][j] = true;
+                } else if(gap == 1) {
+                    if(str[i] == str[j]) {
+                        dp[i][j] = true;
+                        start = i;
                     }
+                } else {
+                    if(str[i] == str[j] && (dp[i + 1][j - 1] == true)) {
+                        dp[i][j] = true;
+                        start = i;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+                
+                if(dp[i][j] && length < gap + 1) {
+                    length = gap + 1;
                 }
             }
         }
-        return s.substr(startIndex, maxlen);
+        
+        return str.substr(start, length);
     }
 };
