9c10c1f7dc9d3c2b4b18dd22b0a1dd34090269ec
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-18 17:22:57 +0530

    Time: 14 ms (73.56%), Space: 7.5 MB (68.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int size = s.size();
+        int maxLen = 1;
         int start = 0;
-        int maxLength = 1;
-        
-        if(size == 1) {
-            return s;
-        }
-        
-        for(int i = 1; i < size; i++) {
-            // For odd length palindrome -> Only one center, so no need of checking of the middle element
-            // Left -> indicates the left pointer to the current center
-			// Right -> indicates the right pointer to the current center
-			
-			int left = i - 1;
-			int right = i + 1;
-			while(left >= 0 && right < size && s[left] == s[right]){
-                int currentLength = right - left + 1;
-                if(currentLength > maxLength) {
+        for(int i = 0; i < s.size(); i++) {
+            // Odd length palindrome
+            int left = i;
+            int right = i;
+            while(left >= 0 && right < s.size() && s[left] == s[right]) {
+                int currLen = right - left + 1;
+                if(maxLen < currLen) {
+                    maxLen = currLen;
                     start = left;
-                    maxLength = currentLength;
-                }
+                }              
                 left--;
                 right++;
             }
             
-            // For even length palindrome -> Two centers, so we need to check for both center elements as well.
-            // Left pointer -> points to the C1 (center 1)
-            // Right pointer -> points to the C2 (center 2)
-			left = i - 1;
-            right = i;
-            while(left >= 0 && right < size && s[left] == s[right]) {
-                int currentLength = right - left + 1;
-                if(currentLength > maxLength) {
+            // Even length palindrome
+            left = i;
+            right = i + 1;
+            while(left >= 0 && right < s.size() && s[left] == s[right]) {
+                int currLen = right - left + 1;
+                if(maxLen < currLen) {
+                    maxLen = currLen;
                     start = left;
-                    maxLength = currentLength;
                 }
                 left--;
                 right++;
-            }            
+            }
         }
-        
-        return s.substr(start, maxLength);
+        return s.substr(start, maxLen);
     }
 };
+
+// babad
+// b, ba, bab, baba...
+// a, bab, baba..
