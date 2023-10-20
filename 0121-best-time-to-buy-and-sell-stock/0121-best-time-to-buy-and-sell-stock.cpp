6a382aa47fd654f8d3cb14d21dd6a8e5fdf8a7c6
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-20 19:41:00 +0530

    Time: 98 ms (45.07%), Space: 93.6 MB (33.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,34 +1,27 @@
+// Return type : int
+// Func name : maxProfit
+// Argument : vector<int> &nums
+
 class Solution {
-public:
-    // Function to calculate the maximum profit
-    int maxProfit(vector<int>& prices) {
-        // Initialize variables
-        int maxProfit = 0;                          // Maximum profit achieved
-        int currentProfit = 0;                      // Current profit at each iteration
-        int minPrice = INT_MAX;                     // Minimum price encountered
-        
-        // Iterate through the prices
-        for (int i = 0; i < prices.size(); i++) {
-            // Update the minimum price encountered so far
-            minPrice = min(minPrice, prices[i]);
+    public :
+        int maxProfit(vector<int> &prices) {
+            int maxProfit = 0;
+            int minValToBuy = INT_MAX;
             
-            // Calculate the profit by subtracting the minimum price from the current price
-            currentProfit = prices[i] - minPrice;
+            for(int i = 0; i < prices.size(); i++) {
+                if(minValToBuy > prices[i]) {
+                    minValToBuy = prices[i];
+                }
+                
+                int currProfit = prices[i] - minValToBuy;
+                
+                if(maxProfit < currProfit) {
+                    maxProfit = currProfit;
+                }
+            }
             
-            // Update the maximum profit if the current profit is higher
-            maxProfit = max(maxProfit, currentProfit);
+            return maxProfit;
         }
-        
-        // Return the maximum profit
-        return maxProfit;
-    }
 };
 
-/*
 
-Time Complexity (TC): O(n)
-
-Space Complexity (SC): O(1)
-
-
-*/
