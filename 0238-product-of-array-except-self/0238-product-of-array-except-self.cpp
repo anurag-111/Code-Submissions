3ca8f045b4d15bc465b8f3d046661a6882c0f9ba
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-21 12:35:54 +0530

    Time: 18 ms (63.54%), Space: 24.4 MB (56.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,20 @@
 class Solution {
     public : 
         vector<int> productExceptSelf(vector<int> &nums) {
-            vector<int> prefixSum(nums.size(), 1);
-            vector<int> suffixSum(nums.size(), 1);
             vector<int> resultSum(nums.size(), 1);
             int product = 1;
             
             // Prefix traversal
             for(int i = 0; i < nums.size(); i++) {
-                prefixSum[i] = prefixSum[i] * product;
+                resultSum[i] = resultSum[i] * product;
                 product = product * nums[i];
             }
             
             product = 1;
             // Suffix traversal
             for(int i = nums.size() - 1; i >= 0; i--) {
-                suffixSum[i] = suffixSum[i] * product;
+                resultSum[i] = resultSum[i] * product;
                 product = product * nums[i];
-            }
-            
-            // Calculation of result traversal
-            for(int i = 0; i < nums.size(); i++) {
-                resultSum[i] = prefixSum[i] * suffixSum[i];
             }
             
             return resultSum;
