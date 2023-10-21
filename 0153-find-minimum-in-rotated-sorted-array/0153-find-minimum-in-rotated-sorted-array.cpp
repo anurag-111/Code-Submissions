e31c0de89ea21b24ad306c51485b02b2d27e7c02
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-22 00:40:14 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (17.84%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,25 +4,16 @@ public:
         int low = 0;
         int high = nums.size() - 1;
         
-        // If no rotations are done
-        if(nums[low] <= nums[high]) {
-            return nums[0];
-        }
-        
-        while(low <= high) {
+        while(low < high) {
             int mid = (low + high) / 2;
             
-            if(nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            } else if(nums[mid] < nums[mid - 1]) {
-                return nums[mid];
-            } else if(nums[low] <= nums[mid]) {
+            if(nums[mid] > nums[high]) {
                 low = mid + 1;
-            } else if(nums[mid] <= nums[high]) {
-                high = mid - 1;
+            } else {
+                high = mid;
             }
         }
         
-        return -1;
+        return nums[low];
     }
 };
