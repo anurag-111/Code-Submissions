e93bd80a1b6f1bee5992202c3cf3cb8e4f3c8491
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-22 15:53:04 +0530

    Time: 84 ms (93.70%), Space: 24.4 MB (67.18%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> triplets;
+        sort(nums.begin(), nums.end());
         
-        for(int i = 0; i < nums.size() - 2; i++) {
-            // Ignoring the 1st duplicative element
+        for(int i = 0; i < nums.size(); i++) {
+            // Ignoring the first duplicatie element
             if(i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             
             int left = i + 1;
             int right = nums.size() - 1;
-            int target = - (nums[i]);
+            int target = -(nums[i]);
             
             while(left < right) {
                 int sum = nums[left] + nums[right];
                 if(sum == target) {
+                    // Found the triplet
                     triplets.push_back({nums[i], nums[left], nums[right]});
                     
-                    // Ignoring the 2nd duplicative element
+                    // Ignoring the second duplicatie element
                     while(left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
                     
-                    // Ignoring the 3rd duplicative element
+                    // Ignoring the third duplicative element
                     while(left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
                     
                     left++;
-                    right--;                       
+                    right--;
                 } else if(sum > target) {
                     right--;
                 } else {
