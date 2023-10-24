b17e7758f4989da2f548c4292d73b1873c653059
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-24 19:37:29 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (19.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,26 +1,23 @@
 class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> charStack;
-        
-        for(int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-            if(ch == '(' || ch == '{' || ch == '[') {
-                charStack.push(ch);
-            } 
-            else if(charStack.empty()) {
-                return false;
-            } else {
-                 if(ch == ')' && charStack.top() == '(' || ch == '}' && charStack.top() == '{' || ch == ']' && charStack.top() == '[') {
-                    // Brackets matched
-                     charStack.pop();
-                } else {
-                    // Brackets not matched 
-                    return false;
-                 }
-            }
+    private:
+        bool matchFunc(char open, char close) {
+            return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
         }
-        
-        return charStack.empty();
-    }
+    public:
+        bool isValid(string s) {
+            stack<char> charStack;
+            for(int i = 0; i < s.size(); i++) {
+                char ch = s[i];
+                if(ch == '(' || ch == '{' || ch == '[') {
+                    charStack.push(ch);
+                } else {
+                    if(charStack.empty() || !matchFunc(charStack.top(), ch)) {
+                        return false;
+                    }
+
+                    charStack.pop();
+                }
+            }
+            return charStack.empty();
+        }
 };
