7cf574b8f7edfe1c2efcd5104200bc22128fa04e
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-25 01:47:59 +0530

    Time: 9 ms (53.46%), Space: 12.4 MB (18.55%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,45 +1,28 @@
 class Solution {
-private:
-    // Function to perform arithmetic operations
-    int performOperation(char op, int operand1, int operand2) {
-        switch (op) {
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '*':
-                return operand1 * operand2;
-            case '/':
-                return operand1 / operand2;
-            default:
-                return 0;
-        }
-    }
-
 public:
-    // Function to evaluate Reverse Polish Notation expression
     int evalRPN(vector<string>& tokens) {
-        stack<int> evalStack;
-
-        for (int i = 0; i < tokens.size(); i++) {
-            // If the token is an operator
-            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
-                int operand2 = evalStack.top();
-                evalStack.pop();
-                int operand1 = evalStack.top();
-                evalStack.pop();
-
-                // Perform the operation and push the result back to the stack
-                int result = performOperation(tokens[i][0], operand1, operand2);
-                evalStack.push(result);
+        stack<long> st;
+        for(auto x : tokens) {
+            if(x == "+" || x == "-" || x == "*" || x == "/") {
+                long operand2 = st.top();
+                st.pop();
+                long operand1 = st.top();
+                st.pop();
+                if(x == "+") {
+                    st.push(operand1 + operand2);
+                } else if(x == "-") {
+                    st.push(operand1 - operand2);
+                } else if(x == "*") {
+                    st.push(operand1 * operand2);
+                } else if(x == "/") {
+                    st.push(operand1 / operand2);
+                }
             } else {
-                // If the token is a number, convert it to integer and push to the stack
-                int num = stoi(tokens[i]);
-                evalStack.push(num);
+                long num = stoi(x);
+                st.push(num);
+                
             }
         }
-
-        // The final result is at the top of the stack
-        return evalStack.top();
+        return st.top();
     }
 };
