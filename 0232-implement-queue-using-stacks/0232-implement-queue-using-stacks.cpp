3020e29966f5d53ffa0382997726042859c1b3eb
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-25 00:05:27 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (50.62%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -33,31 +33,6 @@ public:
     }
 };
 
-/*
-    1,4,5,6
-    
-    stack 1 : 1 -> 4 -> 5 -> 6
-    stack 2 : 6 -> 5 -> 4 -> 1
-    pop : 1
-    stack 2 : 6 -> 5 -> 4 
-    push : 7
-    stack 1 : 4 -> 5 -> 6 -> 7
-    stack 2 : 7 -> 6 -> 5 -> 4
-    pop : 4
-    stack 2 : 7 -> 6 -> 5
-    print stack (queue implemented) : 5 -> 6 -> 7
-    
-    queue : 1 -> 4 -> 5 -> 6 
-    pop : 1
-    push : 7
-    queue : 4 -> 5 -> 6 -> 7
-    pop : 4
-    queue : 5 -> 6 -> 7
-    
-*/ 
-
-
-
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue* obj = new MyQueue();
