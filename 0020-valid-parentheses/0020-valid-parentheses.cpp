66ed94a0c4339aa4506d684bf94fb91dacbcecbc
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-24 19:23:56 +0530

    Time: 3 ms (44.99%), Space: 6.7 MB (19.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st; // Create a stack to store opening brackets.
+        stack<char> charStack;
         
         for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                // If the current character is an opening bracket, push it onto the stack.
-                st.push(s[i]);
+            char ch = s[i];
+            if(ch == '(' || ch == '{' || ch == '[') {
+                charStack.push(ch);
+            } 
+            else if(charStack.empty()) {
+                return false;
             } else {
-                // If the current character is a closing bracket, check if it matches the top of the stack.
-                if(st.empty() || (st.top() == '(' && s[i] != ')') || (st.top() == '{' && s[i] != '}') || (st.top() == '[' && s[i] != ']')) {
-                    return false; // Return false if there is a mismatch or the stack is empty.
-                }
-                st.pop(); // Pop the matching opening bracket from the stack.
+                 if(ch == ')' && charStack.top() == '(' || ch == '}' && charStack.top() == '{' || ch == ']' && charStack.top() == '[') {
+                    // Brackets matched
+                     charStack.pop();
+                } else {
+                    // Brackets not matched 
+                    return false;
+                 }
             }
         }
-
-        return st.empty(); // If the stack is empty, all brackets are matched and valid.
+        
+        return charStack.empty();
     }
 };
-// Time complexity: O(n), where n is the length of the input string s.
-// Space complexity: O(n) due to the stack used to store opening brackets.
