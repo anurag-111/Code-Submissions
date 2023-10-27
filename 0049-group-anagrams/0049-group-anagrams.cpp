5c8a6025c9315da9d370841408ff15dc320eb9b0
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-27 19:37:19 +0530

    Time: 54 ms (13.95%), Space: 25.3 MB (13.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,49 +1,51 @@
-#include <vector>
-#include <string>
-#include <unordered_map>
+// Your input: ["eat","tea","tan","ate","nat","bat"]
+
+// stdout : key: a1e1t1
+//          key: a1e1t1
+//          key: a1n1t1
+//          key: a1e1t1
+//          key: a1n1t1
+//          key: a1b1t1
+         
+// Output: [["bat"],["tan","nat"],["eat","tea","ate"]]
+
 
 class Solution {
-private:
-    // Helper function to generate an anagram key for a given string
-    string generateAnagramKey(string& str) {
-        int characterCounts[26] = {0};
-
-        // Count the frequency of each character in the string
-        for (char ch : str) {
-            characterCounts[ch - 'a']++;
-        }
-
-        // Generate the anagram key based on the sorted counts
-        string key = "";
-        for (int i = 0; i < 26; i++) {
-            if (characterCounts[i] > 0) {
-                key += ('a' + i);
-                key += to_string(characterCounts[i]);
+    private:
+        string generateKey(string str) {
+            vector<int> freq(26, 0);
+            for(int i = 0; i < str.size(); i++) {
+                freq[str[i] - 'a']++;
             }
+            
+            string key = "";
+            for(int i = 0; i < 26; i++) {
+                if(freq[i] > 0) {
+                    key += (i + 'a');
+                    key += to_string(freq[i]);
+                }
+            }
+            
+            cout << key << "key: " << endl;
+            return key;
         }
-        
-        cout << "key: " << key << endl;
-        return key;
-    }
-
-public:
-    // Function to group anagrams together
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> anagramGroups;
-        unordered_map<string, vector<string>> anagramMap;
-
-        // Group strings by their anagram keys
-        for (string str : strs) {
-            string currentString = str;
-            string key = generateAnagramKey(currentString);
-            anagramMap[key].push_back(currentString);
+    
+    public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            // Result
+            vector<vector<string>> groupAnagrams;
+            // Mapping
+            unordered_map<string, vector<string>> bucketMap;
+            
+            for(string str : strs) {
+                string key = generateKey(str);
+                bucketMap[key].push_back(str);
+            }
+            
+            for (pair<string, vector<string>> group : bucketMap) {
+                groupAnagrams.push_back(group.second);
+            }
+            
+            return groupAnagrams;
         }
-
-        // Collect the groups of anagrams
-        for (auto group : anagramMap) {
-            anagramGroups.push_back(group.second);
-        }
-
-        return anagramGroups;
-    }
 };
