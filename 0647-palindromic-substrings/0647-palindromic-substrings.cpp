339efaa7b138cda8a869b88dc652c505b56db1d1
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-27 18:00:00 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (76.74%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int count = 0;
-        if(s.size() == 1) {
-            return 1;
+        if(s.size() <= 1) {
+            return s.size();
         }
         
-        for(int i = 0;  i < s.size(); i++) {
+        int start = 0;
+        int count = 0;
+        int maxLen = 1;
+        for(int i = 0; i < s.size(); i++) {
+            // Odd palindrome length
             int left = i;
             int right = i;
             while(left >= 0 && right < s.size() && s[left] == s[right]) {
+                int currentLen = right - left + 1;
                 count++;
+                if(currentLen > maxLen) {
+                    maxLen = currentLen;
+                    start = left;
+                }
                 left--;
                 right++;
             }
             
+            // Even palindrome length
             left = i;
             right = i + 1;
             while(left >= 0 && right < s.size() && s[left] == s[right]) {
+                int currentLen = right - left + 1;
                 count++;
+                if(currentLen > maxLen) {
+                    maxLen = currentLen;
+                    start = left;
+                }
                 left--;
                 right++;
             }
