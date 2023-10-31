e285166bd1fd7a4ac74e3139d93b2b85c9aea88b
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-10-31 18:40:57 +0530

    Time: 3 ms (95.77%), Space: 7.6 MB (47.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,41 +1,39 @@
 class Solution {
-    private:
-        bool allZero(vector<int>& count) {
-            for (int i = 0; i < 26; i++) {
-                if (count[i] != 0) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-    
-    public:
-        bool checkInclusion(string s1, string s2) {
-            int len1 = s1.length(), len2 = s2.length();
-
-            if (len1 > len2) {
+private:
+    bool allZero(vector<int> &freq) {
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] != 0) {
                 return false;
             }
-
-            vector<int> count(26, 0);
-
-            for (int i = 0; i < len1; i++) {
-                count[s1[i] - 'a']++;
-            }
-
-            for (int i = 0; i < len2; i++) {
-                count[s2[i] - 'a']--;
-
-                if (i - len1 >= 0) {
-                    count[s2[i - len1] - 'a']++;
-                }
-
-                if (allZero(count)) {
-                    return true;
-                }
-            }
-
+        }
+        return true;
+    }
+    
+public:
+    bool checkInclusion(string s1, string s2) {
+        int len1 = s1.size(), len2 = s2.size();
+        
+        if(len1 > len2) {
             return false;
         }
+        
+        vector<int> freq(26, 0);
+        for(int i = 0; i < len1; i++) {
+            freq[s1[i] - 'a']++;
+        }
+        
+        for(int i = 0; i < len2; i++) {
+            freq[s2[i] - 'a']--;
+            
+            if(i - len1 >= 0) {
+                freq[s2[i - len1] - 'a']++;
+            }
+            
+            if(allZero(freq)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 };
