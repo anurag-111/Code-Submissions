24be63b39a8a7fbbe4e92b8cef9fd2b38b161965
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-11-10 08:24:11 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (87.96%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,27 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class Solution {
-private:
-    bool helper(TreeNode* root1, TreeNode* root2) {
-        // If both nodes are null, they are equal
-        if(root1 == NULL && root2 == NULL) {
-           return true;
+    private:
+        bool checkNodes(TreeNode *root1, TreeNode *root2) {
+            if(root1 == NULL && root2 == NULL) {
+                return true;
+            }
+            
+            if(root1 == NULL || root2 == NULL || root1 -> val != root2 -> val) {
+                return false;
+            }
+            
+            return checkNodes(root1 -> left, root2 -> left) && checkNodes(root1 -> right, root2 -> right);
+            
+        }    
+    
+    public:
+        bool isSameTree(TreeNode *p, TreeNode *q) {
+            return checkNodes(p, q);
         }
-        
-        // If one of the nodes is null but the other is not, they are not equal
-        if(root1 == NULL || root2 == NULL) {
-           return false;
-        }
-        
-        // If the values of the current nodes are different, they are not equal
-        if(root1->val != root2->val) {
-            return false;
-        } 
-        
-        return helper(root1->left, root2->left) && helper(root1->right, root2->right);
-        
-    }
-        
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        return helper(p, q);
-    }
 };
