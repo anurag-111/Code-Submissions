4177e639539501872885a6c4dc5d7b59f5ddc0dc
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-11-10 15:03:04 +0530

    Time: 23 ms (33.42%), Space: 28.5 MB (21.61%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -9,34 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
-    private:
-        int calculatePathSum(TreeNode *root, int &maxPathSum) {
-            if(root == NULL) {
-                return 0;
-            }
-            
-            int left = calculatePathSum(root -> left, maxPathSum);
-            int right = calculatePathSum(root -> right, maxPathSum);
-            
-            // Calculating the maximum path of child nodes
-            int maxLeftOrRight = max(left, right);
-            
-            // Root + child value or just the root node itself
-            int maxToReturn = max(root -> val + maxLeftOrRight, root -> val);
-            
-            // Where the current sub-tree is the greatest path sum
-            int currentPathSum = max(root -> val + left + right, maxToReturn);
-            
-            maxPathSum = max(maxPathSum, currentPathSum);
-            
-            return maxToReturn;
+public:
+    int calculateSum(TreeNode *root, int &maxPathSum) {
+        if(root == NULL) {
+            return 0;
         }
+        
+        int left = calculateSum(root -> left, maxPathSum);
+        int right = calculateSum(root -> right, maxPathSum);
+        
+        int maxLeftOrRight = max(left, right);
+        
+        int maxToReturn = max(root -> val + maxLeftOrRight, root -> val);
+        
+        int currentPathSum = max(root -> val + left + right, maxToReturn);
+        
+        maxPathSum = max(maxPathSum, currentPathSum);
+        
+        return maxToReturn;
+    }
     
-    public:
-        int maxPathSum(TreeNode* root) {
-            int maxPathSum = root -> val;
-            calculatePathSum(root, maxPathSum);
-            return maxPathSum;
-        }
+    int maxPathSum(TreeNode *root) {
+        int maxPathSum = root -> val;
+        calculateSum(root, maxPathSum);
+        return maxPathSum;
+    }
 };
