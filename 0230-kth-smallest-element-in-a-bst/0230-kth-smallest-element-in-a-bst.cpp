f1a9425f03eef95840065d4276242b0c8e865052
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-11-10 15:23:55 +0530

    Time: 16 ms (34.81%), Space: 24.3 MB (74.67%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,25 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    int nodeValue = 0;
-    
-    void inorderTraversal(TreeNode *root, int &nodeCount, int k) {
-        if(root == NULL) {
-            return;
+    private:
+        void search(TreeNode *root, int k, int &nodeCount, int &value) {
+            if(root == NULL) {
+                return;
+            }
+            
+            search(root -> left, k, nodeCount, value);
+            
+            nodeCount++;
+            if(k == nodeCount) {
+                value = root -> val;
+            }
+            
+            search(root -> right, k, nodeCount, value);
         }
-        
-        inorderTraversal(root -> left, nodeCount, k);
-        
-        nodeCount++;
-        if(nodeCount == k) {
-            nodeValue = root -> val;
-        }
-        
-        inorderTraversal(root -> right, nodeCount, k);
-    }
     
-    int kthSmallest(TreeNode* root, int k) {
-        int nodeCount = 0;
-        inorderTraversal(root, nodeCount, k);
-        return nodeValue;
-    }
+    public:
+        int kthSmallest(TreeNode* root, int k) {
+            int nodeCount = 0;
+            int value;
+            search(root, k, nodeCount, value);
+            return value;
+        }
 };
