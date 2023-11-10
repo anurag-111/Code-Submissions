a897cbc2a154d24e9586af35d931e73c9340a2e6
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-11-10 17:09:55 +0530

    Time: 24 ms (52.96%), Space: 23.5 MB (70.85%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,31 +11,19 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        while (root) {
-            int currentValue = root->val;
-            
-            if (currentValue > p->val && currentValue > q->val) {
-                // Both p and q are in the left subtree.
-                root = root->left;
-            } else if (currentValue < p->val && currentValue < q->val) {
-                // Both p and q are in the right subtree.
-                root = root->right;
-            } else {
-                // Found the lowest common ancestor.
-                break;
-            }
+        if(root == NULL || root -> val == p -> val || root -> val == q -> val) {
+            return root;
         }
-
+        
+        TreeNode *leftVal = lowestCommonAncestor(root -> left, p, q);
+        TreeNode *rightVal = lowestCommonAncestor(root -> right, p, q);
+        
+        if(leftVal == NULL) {
+            return rightVal;
+        } else if(rightVal == NULL) {
+            return leftVal;
+        }
+        
         return root;
     }
 };
-
-// Time Complexity: The time complexity of the lowestCommonAncestor function is O(h), 
-// where "h" is the height of the binary search tree. In the worst case scenario, 
-// the algorithm traverses from the root to the lowest common ancestor, 
-// which takes time proportional to the height of the tree.
-
-// Space Complexity: The space complexity of the lowestCommonAncestor function is O(1). 
-// The algorithm uses a constant amount of extra space for the currentValue variable and 
-// the loop variable root. It does not utilize any additional data structures that grow with 
-// the input size or the height of the tree. Hence, the space complexity is constant.
