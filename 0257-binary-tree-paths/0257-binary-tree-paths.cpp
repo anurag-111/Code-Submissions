8af40e0148bd0eea4886d3f7d717acda56a818c3
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-11-18 19:26:47 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (72.60%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,36 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    // Recursive function to find all paths from the root to leaf nodes
-    vector<string> paths(TreeNode* root, vector<string>& result, string str) {
-        // If the current node is a leaf node, add the current path to the result
-        if (root->left == NULL && root->right == NULL) {
-            result.push_back(str);
+    private:
+        void traversal(TreeNode *root, vector<string> &treePaths, string str) {            
+            if(root == NULL) {
+                return;
+            }
+            
+            str += to_string(root -> val);
+            
+            if(root -> left == NULL && root -> right == NULL) {
+                treePaths.push_back(str);
+            }
+            
+            if(root -> left) {
+                traversal(root -> left, treePaths, str + "->");
+            } 
+            
+            if(root -> right) {
+                traversal(root -> right, treePaths, str + "->");
+            }
+            
+            return;
+        }   
+    
+    public:
+        vector<string> binaryTreePaths(TreeNode* root) {
+            vector<string> treePaths;
+            traversal(root, treePaths, "");
+            return treePaths;
         }
-
-        // If the left child exists, recursively explore the left subtree
-        if (root->left) {
-            paths(root->left, result, str + "->" + to_string(root->left->val));
-        }
-
-        // If the right child exists, recursively explore the right subtree
-        if (root->right) {
-            paths(root->right, result, str + "->" + to_string(root->right->val));
-        }
-
-        return result;
-    }
-
-public:
-    // Function to find all root-to-leaf paths in a binary tree
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> result;
-
-        // If the tree is empty, return an empty result
-        if (root == NULL) {
-            return result;
-        }
-
-        // Start the path with the root value and explore all possible paths
-        return paths(root, result, to_string(root->val));
-    }
 };
