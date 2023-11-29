08d8b35ccd06f8cabf730b2be4ca5efbe2e3065d
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-11-30 00:15:47 +0530

    Time: 38 ms (75.74%), Space: 6.8 MB (97.49%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,6 +3,7 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
+        
         vector<int> lcs(n + 1, 0);
         
         for(int i = 1; i <= m; i++) {
@@ -17,42 +18,7 @@ public:
                 prev = temp;
             }
         }
+        
         return lcs[n];
     }
 };
-
-// class Solution {
-// public:
-//     int longestCommonSubsequence(string text1, string text2) {
-//         int m = text1.length();
-//         int n = text2.length();
-//         vector<vector<int>> lcs(m + 1, vector<int> (n + 1));
-        
-//         for(int i = 0; i <= m; i++) {
-//             for(int j = 0; j <= n; j++) {
-//                 if(i == 0 || j == 0) {
-//                 lcs[i][j] = 0;
-//                 } else if(text1[i - 1] == text2[j - 1]) {
-//                     lcs[i][j] = 1 + lcs[i - 1][j - 1];
-//                 } else {
-//                     lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);
-//                 }
-//             }
-//         }
-        
-//         return lcs[m][n];
-        
-//     }
-// };
-
-
-// The space complexity is where the significant difference lies:
-
-// Original Code:
-
-// Space complexity: O(m * n)
-// It uses a 2D array lcs with dimensions (m+1) x (n+1) to store intermediate results. This results in a space complexity of O(m * n).
-// Optimized Code:
-
-// Space complexity: O(min(m, n))
-// It uses a 1D array dp with dimensions (n+1) to store intermediate results. The space complexity is determined by the smaller of m and n, making it more memory-efficient.
