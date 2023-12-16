9baf809cb9c36d6be118aa50d393279a53d2ba28
Anurag Barthwal <anuragbarthwal11@gmail.com>
2023-12-16 22:50:42 +0530

    Time: 14 ms (69.36%), Space: 7.2 MB (97.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,38 +4,36 @@ public:
         if(s.size() <= 1) {
             return s;
         }
-        int maxLen = 1;
+        
         int start = 0;
+        int maxLen = 1;
         for(int i = 0; i < s.size(); i++) {
-            // Odd length palindrome
+            // For odd length palindrome
             int left = i;
             int right = i;
             while(left >= 0 && right < s.size() && s[left] == s[right]) {
-                int currLen = right - left + 1;
-                if(maxLen < currLen) {
-                    maxLen = currLen;
-                    start = left;
-                }              
-                left--;
-                right++;
-            }
-            
-            // Even length palindrome
-            left = i;
-            right = i + 1;
-            while(left >= 0 && right < s.size() && s[left] == s[right]) {
-                int currLen = right - left + 1;
-                if(maxLen < currLen) {
-                    maxLen = currLen;
+                int currentLen = right - left + 1;
+                if(currentLen > maxLen) {
+                    maxLen = currentLen;
                     start = left;
                 }
                 left--;
                 right++;
             }
+            
+            // For even length palindrome
+            left = i;
+            right = i + 1;
+            while(left >= 0 && right < s.size() && s[left] == s[right]) {
+                int currentLen = right - left + 1;
+                if(currentLen > maxLen) {
+                    maxLen = currentLen;
+                    start = left;
+                }
+                left--;
+                right++;
+            }         
         }
         return s.substr(start, maxLen);
     }
 };
-
-// tc : O(N * N)
-// sc : O(1)
